022e2f8300605e78276937ab50c143f3b7ba1a40
timothyfox443 <timothyfox443@gmail.com>
2021-10-11 12:31:19 -0700

    build failed with no errors so yeah now I need to know wy

--- LearnIdentity/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ LearnIdentity/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+#nullable disable
 
 namespace LearnIdentity.Areas.Identity.Pages.Account
 {
@@ -63,8 +64,9 @@ namespace LearnIdentity.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
-            [EmailAddress]
-            public string Email { get; set; }
+            [Display(Name ="Username")]
+            //[EmailAddress]
+            public string UserName { get; set; }
 
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
@@ -109,7 +111,7 @@ namespace LearnIdentity.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(Input.UserName, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
